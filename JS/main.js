1a859d05f3762114612df59b4ffc178376f57f53
OverLayyyyy <120062675+Na3ar-17@users.noreply.github.com>
2023-03-16 13:35:41 +0200

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -6,9 +6,9 @@ navButton.addEventListener("click", () => {
   nav.classList.toggle("open");
   document.querySelector("html").classList.toggle("hidden");
   if (nav.classList.contains("open")) {
-    navImg.src = "/IMG/icons/nav-close.svg";
+    navImg.src = "./IMG/icons/nav-close.svg";
   } else {
-    navImg.src = "/IMG/icons/nav-open.svg";
+    navImg.src = "./IMG/icons/nav-open.svg";
   }
 });
 
